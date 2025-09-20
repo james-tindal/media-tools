4ff4b6df2c51a219708429dc77596641d163c49a
james-tindal <10291002+james-tindal@users.noreply.github.com>
2025-09-20 03:43:26 +0100

    transcribe: files arg required

--- code/transcribe/command.ts
+++ code/transcribe/command.ts
@@ -4,7 +4,7 @@ import { transcribe } from './transcribe'
 export default class Transcribe extends Command {
   static override strict = false
   static override args = {
-    files: Args.string({ description: 'files to read' }),
+    files: Args.string({ description: 'files to read', required: true }),
   }
   static override description = 'Transcribe audio to text'
   static override examples = [
